640238d1d49e7cd71f189b2d9bb242e373af2ddd
Muhamad Rifqi <148407375+reevchi@users.noreply.github.com>
2024-06-13 15:23:16 +0700

    Update Streamlit_Diagnosis_Penyakit.py

--- Streamlit_Diagnosis_Penyakit.py
+++ Streamlit_Diagnosis_Penyakit.py
@@ -4,20 +4,15 @@ import pickle
 
 # Memuat model, scaler, dan label encoder dari file
 diagnosis_model = pickle.load(open('diagnosis_penyakit.sav','rb'))
-scaler = pickle.load(open('scaler.pkl','rb'))
-label_encoder = pickle.load(open('label_encoder.pkl','rb'))
 
 # Fungsi untuk memprediksi penyakit berdasarkan gejala
 def predict_disease(symptoms):
     # Membuat dataframe dari gejala
     input_data = pd.DataFrame([symptoms])
     
-    # Standarisasi input data
-    input_data_scaled = scaler.transform(input_data)
-    
     # Memprediksi penyakit
     prediction = diagnosis_model.predict(input_data_scaled)
-    return label_encoder.inverse_transform(prediction)[0]
+    return prediction[0]
 
 # Title
 st.title('Aplikasi Diagnosa Penyakit Berbasis Gejala')
